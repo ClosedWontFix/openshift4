b5b228bc626fdf2558420b082db0804f6e4f254b
ClosedWontFix <12750133+ClosedWontFix@users.noreply.github.com>
2023-05-08 14:21:50 -0500

    updated reboot script

--- reboot_ocp_cluster.sh
+++ reboot_ocp_cluster.sh
@@ -20,39 +20,38 @@ for node in ${MASTER_NODES}; do
   echo "Attempting to drain ${node}..."
   oc adm drain ${node} --ignore-daemonsets --delete-emptydir-data --disable-eviction --force
   if [[ $? -eq 0 ]]; then
-                ssh -o StrictHostKeyChecking=no core@${node} "sudo reboot" > /dev/null 2>&1
-                #Need to update this script to drain nodes before rebooting nodes
-                #oc debug node/${node} -- chroot /host reboot
-                sleep 180
-                oc adm uncordon ${node} > /dev/null 2>&1
-                echo
-                until ping -c 1 -t 1 ${node} > /dev/null 2>&1; do
-                        sleep 120
-                done
-        else
-                echo "${node} failed to drain."
-                exit
-        fi
+    ssh -o StrictHostKeyChecking=no core@${node} "sudo reboot" > /dev/null 2>&1
+    #oc debug node/${node} -- chroot /host reboot
+    sleep 180
+    oc adm uncordon ${node} > /dev/null 2>&1
+    echo
+    until ping -c 1 -t 1 ${node} > /dev/null 2>&1; do
+      sleep 120
+    done
+  else
+    echo "${node} failed to drain."
+    exit
+  fi
 done
 
 # Reboot ocs nodes
 if [[ -n ${OCS_NODES} ]]; then
   for node in ${OCS_NODES}; do
-                echo "Attempting to drain ${node}..."
+    echo "Attempting to drain ${node}..."
     oc adm drain ${node} --ignore-daemonsets --delete-emptydir-data --disable-eviction --force
     if [[ $? -eq 0 ]]; then
-                        ssh -o StrictHostKeyChecking=no core@${node} "sudo reboot" > /dev/null 2>&1
-                        #oc debug node/${node} -- chroot /host reboot
-                        sleep 180
-                        oc adm uncordon ${node} > /dev/null 2>&1
-                        echo
-                        until ping -c 1 -t 1 ${node} > /dev/null 2>&1; do
-                                sleep 120
-                        done
-                else
-                        echo "${node} failed to drain."
-                        exit
-                fi
+      ssh -o StrictHostKeyChecking=no core@${node} "sudo reboot" > /dev/null 2>&1
+      #oc debug node/${node} -- chroot /host reboot
+      sleep 180
+      oc adm uncordon ${node} > /dev/null 2>&1
+      echo
+      until ping -c 1 -t 1 ${node} > /dev/null 2>&1; do
+        sleep 120
+      done
+    else
+      echo "${node} failed to drain."
+      exit
+    fi
   done
 else
   echo "No OCS/ODF nodes were detected.  Moving on."
@@ -61,21 +60,21 @@ fi
 # Reboot infra nodes
 if [[ -n ${INFRA_NODES} ]]; then
   for node in ${INFRA_NODES}; do
-                echo "Attempting to drain ${node}..."
+    echo "Attempting to drain ${node}..."
     oc adm drain ${node} --ignore-daemonsets --delete-emptydir-data --disable-eviction --force
     if [[ $? -eq 0 ]]; then
-                        ssh -o StrictHostKeyChecking=no core@${node} "sudo reboot" > /dev/null 2>&1
-                        #oc debug node/${node} -- chroot /host reboot
-                        sleep 120
-                        oc adm uncordon ${node} > /dev/null 2>&1
-                        echo
-                        until ping -c 1 -t 1 ${node} > /dev/null 2>&1; do
-                                sleep 60
-                        done
-                else
-                        echo "${node} failed to drain."
-                        exit
-                fi
+      ssh -o StrictHostKeyChecking=no core@${node} "sudo reboot" > /dev/null 2>&1
+      #oc debug node/${node} -- chroot /host reboot
+      sleep 120
+      oc adm uncordon ${node} > /dev/null 2>&1
+      echo
+      until ping -c 1 -t 1 ${node} > /dev/null 2>&1; do
+        sleep 60
+      done
+    else
+      echo "${node} failed to drain."
+      exit
+    fi
   done
 else
   echo "No infra nodes were detected.  Moving on."
@@ -84,21 +83,21 @@ fi
 # Reboot worker nodes
 if [[ -n ${WORKER_NODES} ]]; then
   for node in ${WORKER_NODES}; do
-                echo "Attempting to drain ${node}..."
+    echo "Attempting to drain ${node}..."
     oc adm drain ${node} --ignore-daemonsets --delete-emptydir-data --disable-eviction --force
     if [[ $? -eq 0 ]]; then
-                        ssh -o StrictHostKeyChecking=no core@${node} "sudo reboot" > /dev/null 2>&1
-                        #oc debug node/${node} -- chroot /host reboot
-                        sleep 60
-                        oc adm uncordon ${node} > /dev/null 2>&1
-                        echo
-                        until ping -c 1 -t 1 ${node} > /dev/null 2>&1; do
-                                sleep 60
-                        done
-                else
-                        echo "${node} failed to drain."
-                        exit
-                fi
+      ssh -o StrictHostKeyChecking=no core@${node} "sudo reboot" > /dev/null 2>&1
+      #oc debug node/${node} -- chroot /host reboot
+      sleep 60
+      oc adm uncordon ${node} > /dev/null 2>&1
+      echo
+      until ping -c 1 -t 1 ${node} > /dev/null 2>&1; do
+        sleep 60
+      done
+    else
+      echo "${node} failed to drain."
+      exit
+    fi
   done
 else
   echo "No worker nodes were detected.  Moving on."
